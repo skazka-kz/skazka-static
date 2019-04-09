408488d29d6599d61476c75c550b8398828c5c7f
Slava Bezgachev <bezgachev@gmail.com>
2019-04-09 11:32:05 +0600

    Latest from lenovo laptop

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,11 +3,18 @@ import styled from "styled-components";
 import Header from "./header";
 import Footer from "./footer";
 
+import leftGraphic from "../images/left_graphic.svg";
+import rightGraphic from "../images/right_graphic.svg";
+
 const SiteWrapper = styled.div``;
+
+const LeftGraphic = styled.img``;
+const RightGraphic = styled.img``;
 
 const Layout = ({ children }) => {
   return (
     <SiteWrapper>
+
       <Header />
       {children}
       <Footer />
