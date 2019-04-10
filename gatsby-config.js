6b245ab1ff979e23900ab3ae556169f3e66dcbca
Slava Bezgachev <bezgachev@gmail.com>
2019-04-10 17:36:49 +0600

    Latest changes, blog will be scrapped

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-styled-components`,
+    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
